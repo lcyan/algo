9342828838a8942f647f467295820c39ec80f1be
yanlc <yanleichang@vip.163.com>
2024-01-11 16:30:32 +0800

    141. 环形链表

---
+++ src/main/java/cn/vvspace/algo/Solution006.java
@@ -0,0 +1,39 @@
+package cn.vvspace.algo;
+
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/linked-list-cycle/description/">141. 环形链表</a>
+ * <p>
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * <p>
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点
+ */
+public class Solution006 {
+
+    public static void main(String[] args) {
+        ListNode list = ListNode.of(3, 2, 0, -4);
+        System.out.println(hasCycle(list));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

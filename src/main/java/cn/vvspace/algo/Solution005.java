c7224838c3eec9f6e2a1f25690260241a4aaa35a
yanlc <yanleichang@vip.163.com>
2024-01-11 16:17:48 +0800

    876. 链表的中间结点

---
+++ src/main/java/cn/vvspace/algo/Solution005.java
@@ -0,0 +1,39 @@
+package cn.vvspace.algo;
+
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/middle-of-the-linked-list/description/">876. 链表的中间结点</a>
+ * <p>
+ * 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[3,4,5]
+ * <p>
+ * 输入：head = [1,2,3,4,5,6]
+ * 输出：[4,5,6]
+ */
+public class Solution005 {
+
+    public static void main(String[] args) {
+        ListNode list1 = ListNode.of(1, 2, 3, 4, 5);
+        ListNode list2 = ListNode.of(1, 2, 3, 4, 5, 6);
+
+        ListNode.print(middleNode(list1));
+        ListNode.print(middleNode(list2));
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}

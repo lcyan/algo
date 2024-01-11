1214125f5b468eb91bdd3be992e5c7aa1d7cddf1
yanlc <yanleichang@vip.163.com>
2024-01-11 18:03:12 +0800

    160. 相交链表

---
+++ src/main/java/cn/vvspace/algo/Solution008.java
@@ -0,0 +1,56 @@
+package cn.vvspace.algo;
+
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/intersection-of-two-linked-lists/description/">160. 相交链表</a>
+ * <p>
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ * <p>
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ */
+public class Solution008 {
+
+    public static void main(String[] args) {
+        ListNode listA = ListNode.of(4, 1, 8, 4, 5);
+        ListNode listB = ListNode.of(5, 6, 1, 8, 4, 5);
+        ListNode listNode = getIntersectionNode(listA, listB);
+        System.out.println(listNode);
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0, lenB = 0;
+
+        for (ListNode p = headA; p != null; p = p.next) {
+            lenA++;
+        }
+
+        for (ListNode p = headB; p != null; p = p.next) {
+            lenB++;
+        }
+
+        ListNode pA = headA, pB = headB;
+        if (lenA > lenB) {
+            for (int i = 0; i < lenA - lenB; i++) {
+                pA = pA.next;
+            }
+        } else {
+            for (int i = 0; i < lenB - lenA; i++) {
+                pB = pB.next;
+            }
+        }
+
+        while (pA != pB) {
+            pA = pA.next;
+            pB = pB.next;
+        }
+
+        return pA;
+    }
+}

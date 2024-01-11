7747bfc741b1b1b176450368913de51addd9e54b
yanlc <yanleichang@vip.163.com>
2024-01-11 16:45:43 +0800

    142. 环形链表II

---
+++ src/main/java/cn/vvspace/algo/Solution007.java
@@ -0,0 +1,52 @@
+package cn.vvspace.algo;
+
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/linked-list-cycle-ii/description/">142. 环形链表II</a>
+ * <p>
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ * <p>
+ * 不允许修改 链表。
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点
+ */
+public class Solution007 {
+
+    public static void main(String[] args) {
+        ListNode list = cn.vvspace.algo.base.ListNode.of(3, 2, 0, -4);
+        ListNode listNode = hasCycle(list);
+        System.out.println(listNode);
+    }
+
+    public static ListNode hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        // 说明没有环
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}

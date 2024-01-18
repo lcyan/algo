58ffb5b3c895981717e14242afd90d9eeee39fb7
yanlc <yanleichang@vip.163.com>
2024-01-18 15:42:08 +0800

    tree: 递归遍历单链表的函数，其实就是把二叉树的每一层抽象理解成单链表的一个节点进行遍历。

--- src/main/java/cn/vvspace/algo/tree/Solution025.java
+++ src/main/java/cn/vvspace/algo/tree/Solution025.java
@@ -2,7 +2,6 @@ package cn.vvspace.algo.tree;
 
 import cn.vvspace.algo.base.TreeNode;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -12,7 +11,7 @@ import java.util.Queue;
  */
 public class Solution025 {
 
-    List<List<Integer>> res = new ArrayList<>();
+    private final List<List<Integer>> res = new LinkedList<>();
 
     public static void main(String[] args) {
         TreeNode root = //
@@ -27,36 +26,11 @@ public class Solution025 {
                                 new TreeNode(9)));
 
         Solution025 solution025 = new Solution025();
-        System.out.println(solution025.levelTraverseV2(root));
+        System.out.println(solution025.levelTraverseV3(root));
     }
 
-    private List<List<Integer>> levelTraverseV2(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        // root 视为第 0 层
-        traverse(root, 0);
-        return res;
-    }
 
-    // 通过前序遍历实现层次遍历
-    // 这个解法是依赖前序遍历自顶向下、自左向右的顺序特点得到了正确的结果。
-    private void traverse(TreeNode root, int depth) {
-        if (root == null) {
-            return;
-        }
-        // 前序位置，看看是否已经存储 depth 层的节点了
-        if (res.size() <= depth) {
-            // 第一次进入 depth 层
-            res.add(new LinkedList<>());
-        }
-        // 前序位置，在 depth 层添加 root 节点的值
-        res.get(depth).add(root.val);
-        traverse(root.left, depth + 1);
-        traverse(root.right, depth + 1);
-    }
-
-    private void levelTraverse(TreeNode root) {
+    private void levelTraverseV1(TreeNode root) {
         if (root == null) {
             return;
         }
@@ -82,5 +56,70 @@ public class Solution025 {
                 }
             }
         }
+    }
+
+    // ----------------------------------
+
+    private List<List<Integer>> levelTraverseV2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // root 视为第 0 层
+        traverseV2(root, 0);
+        return res;
+    }
+
+    // 通过前序遍历实现层次遍历
+    // 这个解法是依赖前序遍历自顶向下、自左向右的顺序特点得到了正确的结果。
+    private void traverseV2(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        // 前序位置，看看是否已经存储 depth 层的节点了
+        if (res.size() <= depth) {
+            // 第一次进入 depth 层
+            res.add(new LinkedList<>());
+        }
+        // 前序位置，在 depth 层添加 root 节点的值
+        res.get(depth).add(root.val);
+        traverseV2(root.left, depth + 1);
+        traverseV2(root.right, depth + 1);
+    }
+
+    // ----------------------------------
+
+    private List<List<Integer>> levelTraverseV3(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        List<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        traverseV3(nodes);
+        return res;
+    }
+
+    // 这个 traverseV3 函数很像递归遍历单链表的函数，其实就是把二叉树的每一层抽象理解成单链表的一个节点进行遍历。
+    private void traverseV3(List<TreeNode> curLevelNodes) {
+        // base case
+        if (curLevelNodes.isEmpty()) {
+            return;
+        }
+        // 前序位置，计算当前层的值和下一层的节点列表
+        List<Integer> nodeValues = new LinkedList<>();
+        List<TreeNode> nextLevelNodes = new LinkedList<>();
+        for (TreeNode node : curLevelNodes) {
+            nodeValues.add(node.val);
+            if (node.left != null) {
+                nextLevelNodes.add(node.left);
+            }
+            if (node.right != null) {
+                nextLevelNodes.add(node.right);
+            }
+        }
+        // 前序位置添加结果，可以得到自顶向下的层序遍历
+        res.add(nodeValues);
+        traverseV3(nextLevelNodes);
+        // 后序位置添加结果，可以得到自底向上的层序遍历结果
+        // res.add(nodeValues);
     }
 }

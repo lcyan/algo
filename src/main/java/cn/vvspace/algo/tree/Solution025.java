e8d4ed2c9f5d38178275a13d820473c4f862981a
yanlc <yanleichang@vip.163.com>
2024-01-18 15:27:15 +0800

    tree: 通过前序遍历实现层次遍历

--- src/main/java/cn/vvspace/algo/tree/Solution025.java
+++ src/main/java/cn/vvspace/algo/tree/Solution025.java
@@ -2,13 +2,17 @@ package cn.vvspace.algo.tree;
 
 import cn.vvspace.algo.base.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
  * <a href="">输入一棵二叉树的根节点，层序遍历这棵二叉树</a>
  */
 public class Solution025 {
+
+    List<List<Integer>> res = new ArrayList<>();
 
     public static void main(String[] args) {
         TreeNode root = //
@@ -23,7 +27,33 @@ public class Solution025 {
                                 new TreeNode(9)));
 
         Solution025 solution025 = new Solution025();
-        solution025.levelTraverse(root);
+        System.out.println(solution025.levelTraverseV2(root));
+    }
+
+    private List<List<Integer>> levelTraverseV2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // root 视为第 0 层
+        traverse(root, 0);
+        return res;
+    }
+
+    // 通过前序遍历实现层次遍历
+    // 这个解法是依赖前序遍历自顶向下、自左向右的顺序特点得到了正确的结果。
+    private void traverse(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        // 前序位置，看看是否已经存储 depth 层的节点了
+        if (res.size() <= depth) {
+            // 第一次进入 depth 层
+            res.add(new LinkedList<>());
+        }
+        // 前序位置，在 depth 层添加 root 节点的值
+        res.get(depth).add(root.val);
+        traverse(root.left, depth + 1);
+        traverse(root.right, depth + 1);
     }
 
     private void levelTraverse(TreeNode root) {

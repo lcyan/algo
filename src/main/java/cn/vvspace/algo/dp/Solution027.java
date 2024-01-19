7857a36e5b8d847161177fec3c485d0cdf1327e6
yanlc <yanleichang@vip.163.com>
2024-01-19 10:46:44 +0800

    dp: dp实现斐波那契数列 V2优化空间复杂度为O(1)

--- src/main/java/cn/vvspace/algo/dp/Solution027.java
+++ src/main/java/cn/vvspace/algo/dp/Solution027.java
@@ -7,7 +7,7 @@ public class Solution027 {
 
     public static void main(String[] args) {
         Solution027 solution027 = new Solution027();
-        System.out.println(solution027.fib(8));
+        System.out.println(solution027.fibV2(8));
     }
 
     public int fib(int n) {
@@ -24,4 +24,22 @@ public class Solution027 {
 
         return dp[n];
     }
+
+    public int fibV2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        // // 分别代表 dp[i - 1] 和 dp[i - 2]
+        int dp_i_1 = 1;
+        int dp_i_2 = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int dp_i = dp_i_1 + dp_i_2;
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i_1;
+    }
+
 }

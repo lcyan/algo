ef39325b74be9773b51ace79a467ecdec19070f7
yanlc <yanleichang@vip.163.com>
2024-01-21 16:05:53 +0800

    backtrace: 51. N 皇后

---
+++ src/main/java/cn/vvspace/algo/bt/Solution030.java
@@ -0,0 +1,61 @@
+package cn.vvspace.algo.bt;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/permutations/description/">46. 全排列</a>
+ * <p>
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ */
+public class Solution030 {
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3};
+        Solution030 solution029 = new Solution030();
+        System.out.println(solution029.permute(nums));
+    }
+
+    /* 主函数，输入一组不重复的数字，返回它们的全排列 */
+    private List<List<Integer>> permute(int[] nums) {
+        Deque<Integer> trace = new LinkedList<>();
+        boolean[] used = new boolean[nums.length + 1];
+        backtrace(nums, trace, used);
+        return res;
+    }
+
+    private void backtrace(int[] nums, Deque<Integer> trace, boolean[] used) {
+        if (trace.size() == nums.length) {
+            res.add(new LinkedList<>(trace));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            trace.add(nums[i]);
+            used[i] = true;
+            backtrace(nums, trace, used);
+            trace.removeLast();
+            used[i] = false;
+
+        }
+    }
+}
---
+++ src/main/java/cn/vvspace/algo/bt/Solution031.java
@@ -0,0 +1,95 @@
+package cn.vvspace.algo.bt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/n-queens/description/">51. N 皇后</a>
+ * <p>
+ * 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ * <p>
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * <p>
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ * <p>
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * <p>
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ * <p>
+ * 输入：n = 1
+ * 输出：[["Q"]]
+ * <p>
+ * 1 <= n <= 9
+ */
+public class Solution031 {
+
+    private final List<List<String>> res = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Solution031 solution031 = new Solution031();
+        System.out.println(solution031.solveNQueens(4));
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chessboard = new char[n][n];
+        for (char[] chars : chessboard) {
+            Arrays.fill(chars, '.');
+        }
+        backtrace(n, 0, chessboard);
+        return res;
+    }
+
+
+    private void backtrace(int n, int row, char[][] chessboard) {
+        if (n == row) {
+            res.add(array2List(chessboard));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, n, chessboard)) {
+                chessboard[row][col] = 'Q';
+                backtrace(n, row + 1, chessboard);
+                chessboard[row][col] = '.';
+            }
+
+        }
+    }
+
+    private boolean isValid(int row, int col, int n, char[][] board) {
+        // 检查列
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // 检查 45度角是否有皇后
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // 检查 135度角是否有皇后
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> array2List(char[][] chessboard) {
+        List<String> list = new ArrayList<>();
+
+        for (char[] c : chessboard) {
+            list.add(String.copyValueOf(c));
+        }
+        return list;
+    }
+}

54c3cb275e5b57ce8c7b89aeab0df2b8d62a9339
yanlc <yanleichang@vip.163.com>
2024-01-22 14:29:38 +0800

    backtrace: 90. 子集 II

---
+++ src/main/java/cn/vvspace/algo/bt/Solution035.java
@@ -0,0 +1,54 @@
+package cn.vvspace.algo.bt;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/subsets-ii/description/">90. 子集 II</a>
+ * <p>
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,2]
+ * 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ */
+public class Solution035 {
+
+    private final List<List<Integer>> res = new LinkedList<>();
+    private final Deque<Integer> track = new LinkedList<>();
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 2};
+        Solution035 solution035 = new Solution035();
+        System.out.println(solution035.subsetsWithDup(nums));
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtrack(nums, 0);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start) {
+        res.add(new LinkedList<>(track));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            track.add(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+}

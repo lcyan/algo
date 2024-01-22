cb6358977f73b72ab73bd5d3edd8c2c5cf7be6e2
yanlc <yanleichang@vip.163.com>
2024-01-22 16:26:31 +0800

    backtrace: 47.全排列 II

---
+++ src/main/java/cn/vvspace/algo/bt/Solution037.java
@@ -0,0 +1,70 @@
+package cn.vvspace.algo.bt;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/permutations-ii/description/">47.全排列 II</a>
+ * <p>
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ */
+public class Solution037 {
+
+    private final List<List<Integer>> res = new LinkedList<>();
+    private final Deque<Integer> track = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Solution037 solution037 = new Solution037();
+        int[] nums = new int[]{1, 1, 2};
+        System.out.println(solution037.permuteUnique(nums));
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] used = new boolean[nums.length + 1];
+        Arrays.sort(nums);
+        backtrack(nums, used);
+        return res;
+    }
+
+    private void backtrack(int[] nums, boolean[] used) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+
+            // 新添加的剪枝逻辑，固定相同的元素在排列中的相对位置
+            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                // 如果前面的相邻相等元素没有用过，则跳过
+                continue;
+            }
+
+
+            track.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, used);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}

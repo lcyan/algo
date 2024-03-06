070fe5254c586b09f7618c2745a5de87c5c10f78
yanlc <yanleichang@vip.163.com>
2024-03-06 22:04:39 +0800

    bst: 701. 二叉搜索树中的插入操作

---
+++ src/main/java/cn/vvspace/algo/tree/Solution033.java
@@ -0,0 +1,28 @@
+package cn.vvspace.algo.tree;
+
+import cn.vvspace.algo.base.TreeNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/insert-into-a-binary-search-tree/">701. 二叉搜索树中的插入操作</a>
+ * <p>
+ * 给定二叉搜索树（BST）的根节点 root 和要插入树中的值 value ，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+ * <p>
+ * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果
+ */
+public class Solution033 {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}

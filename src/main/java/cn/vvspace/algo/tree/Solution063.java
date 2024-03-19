7b01d8347da9c95fbaf6d9fd08e7c7890a68df6c
yanlc <yanleichang@vip.163.com>
2024-03-19 23:11:39 +0800

    tree: 给你一个二叉树的根节点 root，按 任意顺序，返回所有从根节点到叶子节点的路径。

--- src/main/java/cn/vvspace/algo/tree/Solution063.java
+++ src/main/java/cn/vvspace/algo/tree/Solution063.java
@@ -6,7 +6,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * <a href="">给你一个二叉树的根节点 root，按 任意顺序，返回所有从根节点到叶子节点的路径。</a>
+ * <a href="https://leetcode.cn/problems/binary-tree-paths/">257. 二叉树的所有路径</a>
+ * <p>
+ * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
  * <p>
  * 输入：root = [1,2,3,null,5]
  * <p>

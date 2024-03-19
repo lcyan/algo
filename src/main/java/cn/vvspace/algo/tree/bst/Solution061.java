e59c5e441df2aec96b430a822389277103da8435
yanlc <yanleichang@vip.163.com>
2024-03-19 16:34:14 +0800

    bst: 96. 不同的二叉搜索树(增加备忘录)

--- src/main/java/cn/vvspace/algo/tree/bst/Solution061.java
+++ src/main/java/cn/vvspace/algo/tree/bst/Solution061.java
@@ -10,8 +10,10 @@ package cn.vvspace.algo.tree.bst;
  * 1 <= n <= 19
  */
 public class Solution061 {
+    int[][] memo;
 
     public int numTrees(int n) {
+        memo = new int[n + 1][n + 1];
         return count(1, n);
     }
 
@@ -31,6 +33,26 @@ public class Solution061 {
             int right = count(i + 1, hi);
             res += left * right;
         }
+        return res;
+    }
+
+
+    // 增加备忘录，降低时间复杂度
+    int countV1(int lo, int hi) {
+        if (lo > hi) {
+            return 1;
+        }
+
+        int res = 0;
+        if (memo[lo][hi] != 0) {
+            return memo[lo][hi];
+        }
+        for (int i = lo; i <= hi; i++) {
+            int left = countV1(lo, i - 1);
+            int right = countV1(i + 1, hi);
+            res += left * right;
+        }
+        memo[lo][hi] = res;
         return res;
     }
 }

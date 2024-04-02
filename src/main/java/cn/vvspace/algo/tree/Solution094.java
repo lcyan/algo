19c64a75c02b2ed479e05ca4c945ebd9e5ea8408
yanlc <yanleichang@vip.163.com>
2024-04-02 17:21:49 +0800

    tree: 897. 递增顺序搜索树 遍历思路

--- src/main/java/cn/vvspace/algo/tree/Solution094.java
+++ src/main/java/cn/vvspace/algo/tree/Solution094.java
@@ -9,6 +9,15 @@ import cn.vvspace.algo.base.TreeNode;
  */
 public class Solution094 {
 
+    TreeNode dummy = new TreeNode(-1);
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode treeNode = new Solution094().increasingBSTV1(root);
+        System.out.println(treeNode);
+    }
+
+    // 分解问题思路
     public TreeNode increasingBST(TreeNode root) {
         if (root == null) {
             return null;
@@ -31,5 +40,23 @@ public class Solution094 {
 
         p.right = root;
         return left;
+    }
+
+    // 遍历思路
+    public TreeNode increasingBSTV1(TreeNode root) {
+        TreeNode p = this.dummy;
+        traverse(root);
+        return p.right;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        traverse(root.left);
+        dummy.right = new TreeNode(root.val);
+        dummy = dummy.right;
+        traverse(root.right);
     }
 }

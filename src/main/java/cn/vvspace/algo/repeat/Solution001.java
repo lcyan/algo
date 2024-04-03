d8b01ee58392102c5a9932f589e4fcb7a3f47703
yanlc <yanleichang@vip.163.com>
2024-04-03 14:42:26 +0800

    linkList: 21. 合并两个有序链表

---
+++ src/main/java/cn/vvspace/algo/repeat/Solution001.java
@@ -0,0 +1,38 @@
+package cn.vvspace.algo.repeat;
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/merge-two-sorted-lists/">21. 合并两个有序链表</a>
+ */
+public class Solution001 {
+
+    ListNode dummy = new ListNode(-1);
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        ListNode p = dummy;
+
+        while (p1 != null && p2 != null) {
+
+            if (p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return dummy.next;
+    }
+}

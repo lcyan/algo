8f4d64f49fa4593ba6de4622fef75fb2be56cfca
yanlc <yanleichang@vip.163.com>
2024-04-03 14:51:20 +0800

    linkList: 86. 分隔链表

---
+++ src/main/java/cn/vvspace/algo/repeat/Solution002.java
@@ -0,0 +1,36 @@
+package cn.vvspace.algo.repeat;
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/partition-list/">86. 分隔链表</a>
+ */
+public class Solution002 {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyL = new ListNode(-1);
+        ListNode dummyR = new ListNode(-1);
+        ListNode left = dummyL;
+        ListNode right = dummyR;
+
+        ListNode p = head;
+        while (p != null) {
+            if (p.val < x) {
+                left.next = new ListNode(p.val);
+                left = left.next;
+            } else {
+                right.next = new ListNode(p.val);
+                right = right.next;
+            }
+
+            // 不能直接让 p 指针前进，
+            // p = p.next
+            // 断开原链表中的每个节点的 next 指针
+            ListNode tmp = p.next;
+            p.next = null;
+            p = tmp;
+        }
+        left.next = dummyR.next;
+        return dummyL.next;
+    }
+}

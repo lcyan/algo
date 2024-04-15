417ced06eb9b35e8e11fbe72aceafa9899a422ef
yanlc <yanleichang@vip.163.com>
2024-04-15 09:51:21 +0800

    linkList: 876. 链表的中间结点

---
+++ src/main/java/cn/vvspace/algo/repeat/Solution006.java
@@ -0,0 +1,22 @@
+package cn.vvspace.algo.repeat;
+
+import cn.vvspace.algo.base.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/middle-of-the-linked-list/">876. 链表的中间结点</a>
+ */
+public class Solution006 {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return fast;
+
+    }
+}

4e930fd13ef2e8d95333c7793bef241aedca3b28
yanlc <yanleichang@vip.163.com>
2024-04-16 15:41:40 +0800

    tree: 104. 二叉树的最大深度

---
+++ src/main/java/cn/vvspace/algo/repeat/tree/Solution001.java
@@ -0,0 +1,40 @@
+package cn.vvspace.algo.repeat.tree;
+
+import cn.vvspace.algo.base.TreeNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/">104. 二叉树的最大深度</a>
+ */
+public class Solution001 {
+
+    int res = 0;
+    int depth = 0;
+
+    public int maxDepth(TreeNode root) {
+        traverse(root);
+        return res;
+
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        depth++;
+        res = Math.max(depth, res);
+        traverse(root.left);
+        traverse(root.right);
+        depth--;
+    }
+
+    private int traverseV1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftMax = traverseV1(root.left);
+        int rightMax = traverseV1(root.right);
+        return Math.max(leftMax, rightMax) + 1;
+    }
+}

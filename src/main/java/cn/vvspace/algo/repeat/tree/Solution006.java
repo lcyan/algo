7fdb3d848918be9771698104b8ab0e13577570ad
yanlc <yanleichang@vip.163.com>
2024-04-18 12:51:39 +0800

    dp: 509. 斐波那契数

--- src/main/java/cn/vvspace/algo/repeat/tree/Solution006.java
+++ src/main/java/cn/vvspace/algo/repeat/tree/Solution006.java
@@ -23,4 +23,32 @@ public class Solution006 {
         return memo[n];
     }
 
+    public int fib2(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int fib3(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int i_1 = 1;
+        int i_2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int dp_i = i_1 + i_2;
+            i_2 = i_1;
+            i_1 = dp_i;
+        }
+        return i_1;
+    }
+
 }

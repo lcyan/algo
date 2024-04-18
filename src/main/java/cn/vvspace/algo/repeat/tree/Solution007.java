9aa3452504db158422b7bbcd9a8f04483b22957b
yanlc <yanleichang@vip.163.com>
2024-04-18 16:23:38 +0800

    dp: 动态规划

--- src/main/java/cn/vvspace/algo/repeat/tree/Solution007.java
+++ src/main/java/cn/vvspace/algo/repeat/tree/Solution007.java
@@ -74,4 +74,22 @@ public class Solution007 {
         memo[amount] = (res == Integer.MAX_VALUE) ? -1 : res;
         return memo[amount];
     }
+
+    public int dpV3(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
 }

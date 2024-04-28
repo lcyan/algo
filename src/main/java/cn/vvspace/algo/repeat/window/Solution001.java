e9814527d7796f0e318cae45a0dcefb863711aa6
yanlc <yanleichang@vip.163.com>
2024-04-28 16:56:27 +0800

    window: 76. 最小覆盖子串

---
+++ src/main/java/cn/vvspace/algo/repeat/window/Solution001.java
@@ -0,0 +1,59 @@
+package cn.vvspace.algo.repeat.window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-window-substring/">76. 最小覆盖子串</a>
+ */
+public class Solution001 {
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC", t = "ABC";
+        System.out.println(new Solution001().minWindow(s, t));
+    }
+
+    public String minWindow(String s, String t) {
+        int left = 0, right = 0;
+        int valid = 0;
+        // 记录最小覆盖子串的起始索引及长度
+        int start = 0, len = Integer.MAX_VALUE;
+
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while (need.size() == valid) {
+
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
